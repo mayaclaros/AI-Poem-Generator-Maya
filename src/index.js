5343b86bca385da877ab7ab56d4839b47cc8430e
mayaclaros <123900092+mayaclaros@users.noreply.github.com>
2024-09-02 21:33:49 -0400

    Update index.js
    Added function and typewriter and API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "You may write me down in history With your bitter, twisted lies, You may trod me in the very dirt But still, like dust, I'll rise",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
